fec3d4e21db0dc8bb13a9858b74f41fdf337bb82
azxca1731 <azxca1731@gmail.com>
2019-02-26 17:31:18 +0900

    author 추가

--- insta_project/main/models.py
+++ insta_project/main/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     uploadfrom = models.ImageField(upload_to='images/')
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
     
+class Comment(models.Model):
+    comment = models.CharField(max_length=200)
+    pup_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.comment
     
--- insta_project/main/views.py
+++ insta_project/main/views.py
@@ -22,13 +22,12 @@ def new(request):
 def create(request):
     if request.method=="POST":
         form = BlogPost(request.POST, request.FILES)
-        
         if form.is_valid():
             #모델 객체형태 post이지만 저장은 하지마라!
             post = form.save(commit=False)
             # post.image=request.POST['uploadfrom']
             post.uploadfrom = request.FILES['uploadfrom']
-
+            post.author = request.user
             post.pub_date = timezone.now()
             post.body=request.POST['body']
             
