397cb4e3cf218325a6cdc98ea16d580cdf11b0b1
azxca1731 <azxca1731@gmail.com>
2019-02-26 16:48:41 +0900

    allowed host 설정

--- insta_project/instagram/settings.py
+++ insta_project/instagram/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = '@3fu%t_@yu@+3ow_98swp5eu+amu^jds6kw+nwv*#bgt^&9ov1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "likeinsta.ap-northeast-2.elasticbeanstalk.com"
-]
+ALLOWED_HOSTS =  ['*']
 
 
 # Application definition
